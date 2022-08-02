7be5d34a786b9b796fdbe1f505250b49b145c9ff
Lenddy <leme2003@gmail.com>
2022-08-02 13:30:05 -0400

    Boxing/Unboxing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿List<object> objlist = new List<object>();
+﻿// Create an empty List of type object
+
+List<object> objlist = new List<object>();
+// Add the following values to the list: 7, 28, -1, true, "chair"
+
 
 objlist.Add(7);
 objlist.Add(28);
@@ -7,8 +11,10 @@ objlist.Add(true);
 objlist.Add("chair");
 int sum = 0 ;
 
+// Loop through the list and print all values
 foreach(object item in objlist){
     if( item is int){
+        // Add all values that are Int type together and output the sum
         int intnum = (int)item;
         sum += intnum;
         Console.WriteLine($"{item} is an int");
@@ -20,4 +26,5 @@ foreach(object item in objlist){
         Console.WriteLine($"{item} is a string");
     }
 }
+// print all values that are Int type together and output the sum
 Console.WriteLine($"the sum of all the number is {sum} nicely done");
